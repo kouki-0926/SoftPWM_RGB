370804056c56619ee993ae3ec304a125183a2fdc
Terakado Koki <terakou37910@icloud.com>
2024-02-24 06:27:46 +0900

    update examples

--- examples/example_1/example_1.ino
+++ examples/example_1/example_1.ino
@@ -1,29 +1,43 @@
+/** **************************************
+ * @example example_1.ino
+ * @brief   SoftPWM_RGBライブラリの使用例
+ * @author  https://github.com/kouki-0926
+ ************************************** **/
+
 #include <SoftPWM_RGB.hpp>
 
-const int redpin = 11;
-const int greenpin = 12;
-const int bluepin = 13;
+const int redPin = 11;
+const int greenPin = 12;
+const int bluePin = 13;
 
-RGB rgb(redpin, greenpin, bluepin);
+RGB rgb(redPin, greenPin, bluePin);
 
 void setup() { rgb.init(); }
 
-const int delaytime = 500;
+const int delayTime = 250;
 void loop() {
-    rgb.RGB_LED(64, 0, 64, 100);
+    for (int i = 0; i < 255; i++) {
+        rgb.RGB_LED(i, 255 - i, 0, 10);
+    }
+    for (int i = 0; i < 255; i++) {
+        rgb.RGB_LED(0, i, 255 - i, 10);
+    }
+    for (int i = 0; i < 255; i++) {
+        rgb.RGB_LED(255 - i, 0, i, 10);
+    }
 
     int num = 3;
     while (num--) {
-        rgb.SetFadeTime(delaytime, delaytime);
-        rgb.on(delaytime);
-        rgb.off(delaytime);
+        rgb.SetFadeTime(delayTime, delayTime);
+        rgb.on(delayTime);
+        rgb.off(delayTime);
     }
 
     num = 3;
     while (num--) {
         rgb.ResetFadeTime();
-        rgb.on(delaytime);
-        rgb.off(delaytime);
+        rgb.on(delayTime);
+        rgb.off(delayTime);
     }
 
     rgb.Fade(1000);
--- src/SoftPWM_RGB.cpp
+++ src/SoftPWM_RGB.cpp
@@ -1,7 +1,13 @@
+/** **************************************
+ * @file   SoftPWM_RGB.cpp
+ * @brief  SoftPWM_RGBライブラリの実装ファイル
+ * @author  https://github.com/kouki-0926
+ ************************************** **/
+
 #include "SoftPWM_RGB.hpp"
 
 RGB::RGB(const int redPin, const int greenPin, const int bluePin)
-    : m_red_pin(redpin), m_green_pin(greenpin), m_blue_pin(bluepin) {}
+    : m_red_pin(redPin), m_green_pin(greenPin), m_blue_pin(bluePin) {}
 
 void RGB::init() {
     SoftPWMBegin();
@@ -14,32 +20,32 @@ void RGB::RGB_LED(const int redValue, const int greenValue, const int blueValue,
     SoftPWMSet(m_red_pin, redValue);
     SoftPWMSet(m_green_pin, greenValue);
     SoftPWMSet(m_blue_pin, blueValue);
-    delay(delaytime);
+    delay(delayTime);
 }
 
 void RGB::SetFadeTime(const int delayTime_1, const int delayTime_2) {
-    SoftPWMSetFadeTime(ALL, delaytime_1, delaytime_2);
+    SoftPWMSetFadeTime(ALL, delayTime_1, delayTime_2);
 }
 
 void RGB::ResetFadeTime() { SoftPWMSetFadeTime(ALL, 0, 0); }
 
 void RGB::Fade(const int delayTime) {
-    SoftPWMSetFadeTime(ALL, delaytime, delaytime);
-    RGB_LED(255, 0, 0, delaytime);
-    RGB_LED(0, 255, 0, delaytime);
-    RGB_LED(0, 0, 255, delaytime);
+    SoftPWMSetFadeTime(ALL, delayTime, delayTime);
+    RGB_LED(255, 0, 0, delayTime);
+    RGB_LED(0, 255, 0, delayTime);
+    RGB_LED(0, 0, 255, delayTime);
 }
 
 void RGB::on(const int delayTime) {
     SoftPWMSet(m_red_pin, 255);
     SoftPWMSet(m_green_pin, 255);
     SoftPWMSet(m_blue_pin, 255);
-    delay(delaytime);
+    delay(delayTime);
 }
 
 void RGB::off(const int delayTime) {
     SoftPWMSet(m_red_pin, 0);
     SoftPWMSet(m_green_pin, 0);
     SoftPWMSet(m_blue_pin, 0);
-    delay(delaytime);
+    delay(delayTime);
 }
